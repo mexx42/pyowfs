b70919e020300d5d8a6ce4456e13a1bdd426a93f
Marcus Priesch <marcus@priesch.co.at>
2009-12-27 20:30:43 +0000

    doc added
    git-svn-id: svn+ssh://10.3.0.2/home/svn/repos/projects/pyowfs@655 d29aca3a-deec-0310-833d-f8b10a66600f

--- pyowfs/owfs.py
+++ pyowfs/owfs.py
@@ -179,7 +179,104 @@ class Connection (Sensor) :
     It initialises libcapi and connects to the port you specify. For port
     specifications look at the owfs documentation. Examples for valid port's
     are: '192.168.2.112:3030', 'u'.
+
+    typical usage :
+
+    >>> from pyowfs import Connection
+    >>> root = Connection ("192.168.2.112:3030")
+    >>> for s in root.find () : print s
+    ...
+    <Sensor /20.C1A00B000000/ - DS2450>
+    <Sensor /12.E8F672000000/ - DS2406>
+    <Sensor /29.336C08000000/ - DS2408>
+
+    disabling the cache is reflected in the %r of the sensor :
+
+    >>> s.use_cache (0)
+    >>> for s in root.find () : print s
+    ...
+    <Sensor /uncached/20.C1A00B000000/ - DS2450>
+    <Sensor /uncached/12.E8F672000000/ - DS2406>
+    <Sensor /uncached/29.336C08000000/ - DS2408>
+
+    find sensors of a specific type :
+
+    >>> s = root.find (type="DS2406")
+
+    dump all entries of the sensor :
+
+    >>> for e in s.iter_entries () : print e
+    ...
+    PIO.BYTE
+    PIO.ALL
+    PIO.A
+    PIO.B
+    <Dir '/12.E8F672000000/T8A/'>
+    <Dir '/12.E8F672000000/TAI8570/'>
+    address
+    alias
+    channels
+    crc8
+    family
+    flipflop.BYTE
+    flipflop.ALL
+    flipflop.A
+    flipflop.B
+    id
+    latch.BYTE
+    latch.ALL
+    latch.A
+    latch.B
+    locator
+    memory
+    <Dir '/12.E8F672000000/pages/'>
+    power
+    present
+    r_address
+    r_id
+    r_locator
+    sensed.BYTE
+    sensed.ALL
+    sensed.A
+    sensed.B
+    set_alarm
+    type
+
+    access 'memory' :
+
+    >>> s.get ("memory")
+    "h\xaa\xaa\x1d\xc6\x84|\xcd\xa1;P\9d3\xd5\x91" ...
+
+    'pages' is a directory, so lets see whats beneath it :
+
+    >>> s.get ("pages")
+    <Dir '/12.E8F672000000/pages/'>
+    >>> for e in s.get ("pages").iter_entries () : print e
+    ...
+    page.ALL
+    page.0
+    page.1
+    page.2
+    page.3
+
+    access to individual pages :
+
+    >>> s.get ("pages").get ("page.1")
+    '\xff\x00\x8b\xa3J\r\x1e\x84\xcd\x90\x15:\x9d' ...
+    >>> s.get ("pages").get ("page.2")
+    '\xb1k-\x0bQ\x04\xe7\xdfh\xa1\d\xc6\x84|\xcd2' ...
+
+    also possible to access long paths directly via underlying libowcapi :
+
+    >>> root.capi.get ("/bus.0/interface/settings/usb/datasampleoffset")
+    '           8'
+    >>> root.capi.put ("/bus.0/interface/settings/usb/datasampleoffset", "10")
+    True
+    >>> root.capi.get ("/bus.0/interface/settings/usb/datasampleoffset")
+    '          10'
+
     """
+
     def __init__ (self, port) :
         capi = libcapi.CAPI ()
         capi.init (port)
@@ -188,5 +285,3 @@ class Connection (Sensor) :
 # end class Connection
 
 ### __END__ owfs
-
-
